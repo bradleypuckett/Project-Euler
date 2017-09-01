4e80221e2c558619aa0fefce9a921a6f4a0fee09
Bradley Puckett <bpuckett@ucsc.edu>
2017-09-01 11:31:44 -0700

    Create Problem_003.cpp

---
+++ Problem_003.cpp
@@ -0,0 +1,18 @@
+
+int main(){
+    long int max = 0;
+    
+    for(int x = 2; x <= 600851475143; x++){
+        if(isPrime(x)){
+            max = x;
+        }
+    }
+    std::cout << max << std::endl;
+}
+
+bool isPrime(){
+    
+    
+    
+    
+}
